1f5db2d416329f192e588c8809a444b692e5b02c
petra-tuskan <tuskanpetra129@gmail.com>
2022-05-06 02:15:01 +0200

    test

---
+++ main.js
@@ -0,0 +1,13 @@
+const profile = document.querySelector("#profile");
+const heading1 = document.querySelector("#h1");
+const heading2 = document.querySelector("#h2");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+
+gsap.fromTo(heading1, { x: 1500 }, { x: 0, duration: 2, ease: "power3.out" });
+gsap.fromTo(heading2, { y: -1500 }, { y: 0, duration: 3, ease: "power2.out" });
+
+gsap.fromTo(profile, { x: -1500 }, { x: 0, duration: 2.5 });
+
+gsap.fromTo(btn1, { x: -1500 }, { x: 0, duration: 3 });
+gsap.fromTo(btn2, { x: 1500 }, { x: 0, duration: 3 });
