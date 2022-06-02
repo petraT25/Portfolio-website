5ede6ecdfdff950ab0409c70e15f873ba1568aa1
petra-tuskan <tuskanpetra129@gmail.com>
2022-06-02 22:07:32 +0200

    changed news image and animation time

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const heading2 = document.querySelector("#h2");
 const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
 
-gsap.fromTo(heading1, { x: 1500 }, { x: 0, duration: 2, ease: "power3.out" });
-gsap.fromTo(heading2, { y: -1500 }, { y: 0, duration: 3, ease: "power2.out" });
+gsap.fromTo(heading1, { x: 1500 }, { x: 0, duration: 1.5, ease: "power3.out" });
+gsap.fromTo(heading2, { y: -1500 }, { y: 0, duration: 2.2, ease: "power2.out" });
 
-gsap.fromTo(profile, { x: -1500 }, { x: 0, duration: 2.5 });
+gsap.fromTo(profile, { x: -1500 }, { x: 0, duration: 2.2 });
 
-gsap.fromTo(btn1, { x: -1500 }, { x: 0, duration: 3 });
-gsap.fromTo(btn2, { x: 1500 }, { x: 0, duration: 3 });
+gsap.fromTo(btn1, { x: -1500 }, { x: 0, duration: 2 });
+gsap.fromTo(btn2, { x: 1500 }, { x: 0, duration: 2 });
