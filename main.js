e5107d1de6f5db62f9cb91bf97ff0cab10bc9ee7
petra-tuskan <tuskanpetra129@gmail.com>
2022-06-28 20:55:19 +0200

    changed project section

--- main.js
+++ main.js
@@ -5,9 +5,18 @@ const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
 
 gsap.fromTo(heading1, { x: 1500 }, { x: 0, duration: 1.5, ease: "power3.out" });
-gsap.fromTo(heading2, { y: -1500 }, { y: 0, duration: 2.2, ease: "power2.out" });
+gsap.fromTo(
+  heading2,
+  { y: -1500 },
+  { y: 0, duration: 2.2, ease: "power2.out" }
+);
 
 gsap.fromTo(profile, { x: -1500 }, { x: 0, duration: 2.2 });
 
 gsap.fromTo(btn1, { x: -1500 }, { x: 0, duration: 2 });
 gsap.fromTo(btn2, { x: 1500 }, { x: 0, duration: 2 });
+
+
+AOS.init();
+
+
